57b45cdd8cbe5865bde40b7f67b6ca899ad23b67
Enoch Aikpokpodion <enoch_aik@icloud.com>
2023-02-06 03:36:24 +0100

    completed all necessary ball collisions and fixtureDef

--- lib/components/ball.dart
+++ lib/components/ball.dart
@@ -1,94 +1,113 @@
+import 'dart:math';
+
 import 'package:basketball_game/constants/image_assets.dart';
-import 'package:basketball_game/game/basketball_game.dart';
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
-import 'package:flame_forge2d/body_component.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
-import 'package:flutter/gestures.dart';
-import 'package:forge2d/src/dynamics/body.dart';
 
-class BallComponent extends BodyComponent with Draggable {
+class BallComponent extends BodyComponent with Draggable, CollisionCallbacks {
   final Vector2 position;
+  late SpriteComponent _ball;
 
   BallComponent(this.position);
 
-  bool _enableSwipe = true;
-  late Vector2 _drag;
+  bool _canSwipe = true;
+  bool _hasPeaked = false;
+  bool _createNewBall = false;
+  final Random _ballPosition = Random();
 
   @override
   Future<void> onLoad() async {
-    await super.onLoad();
-
-    add(SpriteComponent()
+    _ball = SpriteComponent()
       ..sprite = await gameRef.loadSprite(ImageAssets.basketballSprite)
       ..anchor = Anchor.center
-      ..size = Vector2.all(11));
-    renderBody = true;
+      ..opacity = 1
+      ..size = Vector2.all(11);
 
-    /*position = Vector2(gameRef.size.x / 2, gameRef.size.y - 70);
-    height = 100;
-    width = 100;
-    anchor = Anchor.center;*/
+    await super.onLoad();
+    add(_ball);
+    renderBody = false;
   }
 
   @override
   Body createBody() {
-    Shape shape = CircleShape()..radius = 5;
+    Shape shape = CircleShape()..radius = 5.5;
+    Filter filter = Filter();
+    filter.categoryBits = 0x0002;
+    filter.maskBits = 0x0001;
+    Vector2 ballPos = Vector2(
+        _ballPosition.nextInt(gameRef.size.x ~/ 1.15).toDouble() + 0.2,
+        position.y);
+    print(ballPos);
+    opacity = 0.1;
     BodyDef bodyDef = BodyDef(
-        linearDamping: 0.5,
-        userData: this,
-        angularDamping: .8,
-        position: Vector2(gameRef.size.x / 2, gameRef.size.y),
-        type: BodyType.dynamic);
-    FixtureDef fixtureDef = FixtureDef(
-      shape,
-      friction: 0.4,
-      density: 1.0,
-      restitution: 0.4,
+      linearDamping: 0.5,
+      userData: this,
+      angularDamping: .8,
+      position: ballPos,
+      type: BodyType.dynamic,
     );
+    FixtureDef fixtureDef = FixtureDef(shape,
+        friction: 0.4, density: 10.0, restitution: 0, filter: filter);
     return world.createBody(bodyDef)..createFixture(fixtureDef);
   }
 
-/*
-  @override
-  bool onDragStart(DragStartInfo info) {
-    body.applyLinearImpulse(info.eventPosition.game*1000);
-    return true;
-  }
-*/
-
   @override
   bool onDragUpdate(DragUpdateInfo info) {
-    if (_enableSwipe) {
+    if (_canSwipe) {
       body.applyLinearImpulse(
         Vector2(info.delta.game.x, -4) * 2000,
       );
-      _drag = info.delta.game;
+      _createNewBall = true;
     }
 
-    // print(info.delta.game.toString());
     return true;
   }
 
   @override
   void update(double dt) {
     super.update(dt);
-    //print(body.position.y);
+    double height = body.position.y;
+    double maxHeight = 0; // Maximum height of the screen
+    double minHeight = gameRef.size.y;
+    double ratio = (maxHeight - minHeight) / (height - minHeight);
+    double size = min(max(ratio, 0.0), 1.0);
+    _ball.width = size * 11;
+
+    _ball.height = size * 11;
+    body.sleepTime = 2;
     if (body.position.y <= 55) {
-      _enableSwipe = false;
+      _canSwipe = false;
       //print('reached limit');
       if (body.position.y <= 20) {
         body.applyForce(
             Vector2(0, 3000 * body.position.distanceTo(Vector2(0, 0))),
             point: body.position);
+        _hasPeaked = true;
+        _createNewBall ? gameRef.add(BallComponent(position)) : null;
+        _createNewBall = false;
       }
-    } else {
-      // _enableSwipe = true;
+    }
+    if (_hasPeaked && !_canSwipe && body.position.y >= gameRef.size.y - 30) {
+      if (_ball.opacity > 0.02) {
+        _ball.opacity -= dt / 2.3;
+      }
+      /*body.position.y >= gameRef.size.y - 7
+          ? body.setAwake(false)
+          : null;*/
+      if (!body.isAwake || !body.isActive) {
+        world.destroyBody(body);
+        gameRef.remove(this);
+      }
+
+      //game.add(BallComponent(position));
     }
   }
 
   @override
-  bool onDragEnd(DragEndInfo info) {
-    return true;
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollision(intersectionPoints, other);
+    if (other is BallComponent) {}
   }
 }
--- lib/game/basketball_game.dart
+++ lib/game/basketball_game.dart
@@ -1,7 +1,5 @@
-import 'dart:ui';
 
 import 'package:basketball_game/components/ball.dart';
-import 'package:basketball_game/components/basketball_stand.dart';
 import 'package:basketball_game/components/boundary.dart';
 import 'package:basketball_game/components/mural_component.dart';
 import 'package:basketball_game/constants/image_assets.dart';
@@ -9,27 +7,24 @@ import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
 
-class BasketBallGame extends Forge2DGame with HasDraggables {
+class BasketBallGame extends Forge2DGame with HasDraggables,HasCollisionDetection {
   BasketBallGame() {
     //World world = World(Vector2(0,0));
   }
 
   @override
   Future<void> onLoad() async {
+
+
     await super.onLoad();
     final boundaries = createBoundaries(this);
     boundaries.forEach(add);
     Vector2 gameSize = screenToWorld(camera.viewport.effectiveSize);
-    addAll([
+    add(
       MuralComponent(),
-
-      BallComponent(gameSize),
       //BasketBallStandComponent(),
-    ]);
+    );
+    add(BallComponent(gameSize,));
   }
 
-  /*@override
-  void update(double dt) {
-    super.update(dt);
-  }*/
 }
